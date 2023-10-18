f7a8d200756ee2734a770abf43f6b344997858aa
Prud3266 <prud3266@gmail.com>
2023-10-18 16:27:10 +0100

    modified:   server.js 	modified:   controller/signupController.js

--- server.js
+++ server.js
@@ -19,10 +19,6 @@ app.use('/login', require('./routes/login.js'))
 const URL = process.env.URL
 const PORT = process.env.PORT
 
-// app.listen(PORT,()=>{
-//     console.log(`Server Running on ${PORT}...`);
-// })
-
 mongoose.connect(URL).then(()=>{
     console.log("Connected to MongoDB")
     app.listen(PORT,()=>{
